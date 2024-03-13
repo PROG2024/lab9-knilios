11f5a41d6c4ab1a68be577eda3ec18bfb163b4dc
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-03-13 11:51:21 +0700

    Finished task 3

--- counter/counter.py
+++ counter/counter.py
@@ -8,15 +8,27 @@
    1. in Counter, do not use any static methods except __new__.
       You may not have a __new__ depending on how you implement the singleton.
 """
+from typing import Self
 
 class Counter:
-
+    __instance = None
+    
     def __init__(self):
         self.__count = 0
 
-
+    @staticmethod
+    def __new__(cls, *args, **kwargs) -> Self:
+        if cls.__instance is None:
+            cls.__instance = super(Counter, cls).__new__(cls)
+        return cls.__instance
+        
     def __str__(self):
         return f"{self.__count}"
 
-    #TODO write count property
-    #TODO write increment method
+    @property
+    def count(self):
+        return self.__count
+
+    def increment(self):
+        self.__count += 1
+        return self.count
--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,44 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+class Test(unittest.TestCase):
+   def setUP(self):
+      self.instance = Counter()
+      
+   def test_both_have_the_same_count(self):
+      a = Counter()
+      a.increment()
+      a.increment()
+      b = Counter()
+      b.increment()
+      self.assertEqual(a.count, b.count)
+   
+   def test_both_instance_are_the_same(self):
+      a = Counter()
+      b = Counter()
+      b.increment()
+      self.assertIs(a, b)
+   
+   def test_check_memory_address(self):
+      a = Counter()
+      b = Counter()
+      self.assertEqual(id(a), id(b))
+      
+   def test_counter_initiate_with_count_is_zero(self):
+      a = Counter()
+      self.assertEqual(0, a.count)
+      
+   def test_counter_counting_properly(self):
+      a = Counter()
+      a.increment()
+      a.increment()
+      self.assertEqual(2, a.count)
+            
+      
+if __name__ == "__main__":
+   unittest.main()
+      
+
